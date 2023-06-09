e187a27bb43811790e3dd99c6400ab0ca3321c30
Romeo Hernandez <romeo.hernandez@Mac-mini-de-Estudio.local>
2023-06-09 16:54:28 -0600

    Commit Junio 9 2023 Bla

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,15 +1,9 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 import deleteIcon from '../assets/delete.svg'
-import newIcon from '../assets/new.svg'
+// import newIcon from '../assets/new.svg'
 import editIcon from '../assets/edit.svg'
 
-const initialTodo = [
-	{ id: 1, title: "Todo #1" },
-	{ id: 2, title: "Todo #2" },
-	{ id: 3, title: "Todo #3" },
-	{ id: 4, title: "Todo #4" },
-	{ id: 5, title: "Todo #5" },
-]
+const initialTodo = []
 
 const type = {
     ADD: 'ADD',
@@ -20,7 +14,15 @@ const type = {
 const reducer = (state, action) => {
     switch (action.type) {
         case type.DELETE:
-            return state.filter(todo => todo.id !== action.id)
+            return state.filter(todo => todo.id !== action.payload)
+        
+        case type.ADD:
+            return [ ...state, action.payload ]
+
+        case type.EDIT: {
+            const todoEdited = action.payload
+            return state.map(todo => todo.id === todoEdited.id ? todoEdited : todo )
+        }
         
     
         default:
@@ -31,38 +33,72 @@ const reducer = (state, action) => {
 const TodoApp = () => {
 
     const [ todos, dispatch ] = useReducer(reducer, initialTodo)
-    
-  return (
-    <div className="todo">
-        <h3>TodoApp</h3>
-        <ul>
-            {
-                todos.map(todo => (
+    const [ text, setText ] = useState("")
 
-                <li key={todo.id}>
-                    <div className="card">
-                        <div className="id">
-                            <h4>{todo.id}</h4>
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+
+        if(inputVerification()) {
+            const newTodo = { id: Date.now(), title: text.trim() }
+            dispatch( { type: type.ADD, payload: newTodo } )
+            setText("")
+        }
+
+    }
+
+    const handleEdit = (todo) => {
+
+        if(inputVerification()) {
+            dispatch( { type: type.EDIT, payload: { ...todo, title: text.trim() } } )
+            setText("")
+        }
+    }
+
+    const inputVerification = () => {
+        // const inputText = text.trim()
+        
+        if(text.trim() !== "") {
+            setText(text.trim())
+            return true
+        }
+
+        setText("")
+        return false
+    }
+    
+    return (
+        <div className="todo">
+            <h3>TodoApp</h3>
+            <form onSubmit={handleSubmit}>
+                <input className='inputText' value={text} onChange={e => setText(e.target.value)} placeholder='New todo' />
+                <button type="submit">Add</button>
+            </form>
+            <ul>
+                {
+                    todos.map(todo => (
+
+                    <li key={todo.id}>
+                        <div className="card">
+                            <div className="id">
+                                <h6>ID: {todo.id}</h6>
+                            </div>
+                            <h3>{todo.title}</h3>
+                            <div className='btns'>
+                                <button onClick={() => handleEdit(todo)}>
+                                    <img src={editIcon} alt="" />
+                                </button>
+                                <button onClick={() => dispatch( { type: type.DELETE, payload: todo.id } )}>
+                                    <img className='delete-icon' src={deleteIcon} alt="" />
+                                </button>
+                            </div>
                         </div>
-                        <h3>{todo.title}</h3>
-                        <div className='btns'>
-                            <button onClick={() => dispatch( { type: type.ADD, id: todo.id } )}>
-                                <img src={newIcon} alt="" />
-                            </button>
-                            <button onClick={() => dispatch( { type: type.ADD, id: todo.id } )}>
-                                <img src={editIcon} alt="" />
-                            </button>
-                            <button onClick={() => dispatch( { type: type.DELETE, id: todo.id } )}>
-                                <img className='delete-icon' src={deleteIcon} alt="" />
-                            </button>
-                        </div>
-                    </div>
-                </li>
-                ))
-            }
-        </ul>
-    </div>
-  )
+                    </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default TodoApp
